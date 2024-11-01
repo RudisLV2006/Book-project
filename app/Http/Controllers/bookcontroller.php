d9b3cc0cce7dd91bbbe78042c4caf2a22e471c78
kristaps <you@example.com>
2024-11-01 11:17:36 +0200

    Rename book and bookcontroller to notation rules; get index view;

--- app/Http/Controllers/bookcontroller.php
+++ app/Http/Controllers/bookcontroller.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
-class bookcontroller extends Controller
+class BookController extends Controller
 {
-    //
+    public function index(Book $book)
+    {
+        return 'index';
+    }
 }
